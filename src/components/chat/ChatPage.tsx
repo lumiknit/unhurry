fa237427085c14848193fa3fafa0e18954368a1e
lumiknit <aasr4r4@gmail.com>
2025-04-28 20:42:48 +0900

    Fix minors

--- src/components/chat/ChatPage.tsx
+++ src/components/chat/ChatPage.tsx
@@ -6,7 +6,6 @@ import ChatHistoryView from './ChatHistoryView';
 const ChatPage: Component = () => {
 	return (
 		<>
-			<div class="top-pad" />
 			<div class="container p-1">
 				<ChatHistoryView />
 			</div>
